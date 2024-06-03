4e3ee43d0dd46cb6250d1ceee080937cc72c6b37
Soane3302116 <3302116@spsd.sk.ca>
2024-06-03 15:21:55 -0600

    fixed error

---
+++ david work/user.py
@@ -0,0 +1,7 @@
+class User(object):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        
+    def hash_password():
+        pass
