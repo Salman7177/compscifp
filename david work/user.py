617f1698126912e4b88cbba29c3285f7619b2674
Soane3302116 <3302116@spsd.sk.ca>
2024-06-04 14:47:11 -0600

    added User class file

--- david work/user.py
+++ david work/user.py
@@ -1,7 +1,10 @@
-class User(object):
+class UserObject(object):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+    
+    def test_func(self):
+        print(self.username)
         
-    def hash_password():
-        pass
+    # def hash_password():
+    #     pass
