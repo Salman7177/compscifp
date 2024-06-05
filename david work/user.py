5f671a24464c64f34a6091519d27a8d918c57a42
Soane3302116 <3302116@spsd.sk.ca>
2024-06-05 15:23:04 -0600

    attempted to fix overwriting

--- david work/user.py
+++ david work/user.py
@@ -1,38 +1,33 @@
 import json
-from pathlib import Path
-
 class UserObject(object):
     def __init__(self, username, password):
         self.username = username
         self.password = password
     
     def convert_to_json(self):
-        output = createWriter("data/UserInformation.json")
-        output_path = Path("data/UserInformation.json")
+        output = createWriter("UserInformation.json")
         
-        if output_path.is_file():
-            with open("data/UserInformation.json", "a") as userinfo:
-                
+        with open("UserInformation.json", "a") as userinfohere:
                 User = {
                         "username": self.username,
                         "password": self.password
                         }
                 jsonified_user = json.dumps(User)
                 print(jsonified_user)
-                userinfo.wirte(jsonified_user)
+                userinfohere.write(jsonified_user)
                 
-        else:
-            User = {
-                "username": self.username,
-                "password": self.password
-                }
+        # else:
+        #     User = {
+        #         "username": self.username,
+        #         "password": self.password
+        #         }
             
-            jsonified_user = json.dumps(User)
-            userinfo.wirte(jsonified_user)
+        #     jsonified_user = json.dumps(User)
+        #     userinfo.wirte(jsonified_user)
                 
-            output.append(jsonified_user)
-            output.flush()
-            output.close()
+        #     output.append(jsonified_user)
+        #     output.flush()
+        #     output.close()
             
         
     def hash_password(self):
