a1da13af0f97a015a4303322230d1052c369b18a
Salman7177 <1003392@spsd.sk.ca>
2024-06-09 17:25:24 -0600

    blah blah

--- postFunction/post.py
+++ postFunction/post.py
@@ -15,16 +15,6 @@ class postFunction(object):
         print(post)
         
         with open("data/posts.json", "a") as file_json:
-            # Convert the post_dict to JSON and write it to the file
             json.dump(post_dict, file_json, indent=4)
             file_json.write("\n")
-        
-        # jsonified_posts = json.dumps(posts_list, indent=4)
-        
-        # print(jsonified_posts)
-        
-        # file_json.write(str(jsonified_posts))
-        # file_json.flush()
-        # file_json.close()
-        
         
