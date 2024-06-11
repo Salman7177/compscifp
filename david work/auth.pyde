eb48ff25210572639dad73f7af9156906b877f99
Soane3302116 <3302116@spsd.sk.ca>
2024-06-11 14:53:43 -0600

    added early version of password hashing

--- david work/auth.pyde
+++ david work/auth.pyde
@@ -13,37 +13,43 @@ def setup():
 def draw():
     global username
     if userLoggedIn == False:
-        background(0)
+        background(240)
         
         textSize(20)
+        
+        fill(0)
         text("Social Media Site", 620, 40)
         
         # Form
+        fill(0)
         text("Username: ", 400, 300)
         fill(255)
         rectMode(CENTER)
         rect(600, 295, 200, 30)
+        fill(0)
         
-        
+        fill(0)
         text("Password: ", 400, 400)
+        fill(255)
         rect(600, 395, 200, 30)
+
         
-        xusername = 600
+        xusername = 510
         
         for letter in username:
             
-            fill(255, 0, 0)
-            text(letter, xusername, 295)
+            fill(0)
+            text(letter, xusername, 300)
             xusername += 10
+
             
         xpassword = 500
         for letter in password:
             
-            fill(255, 0, 0)
+            fill(0)
             textSize(80)
-            text(".", xpassword, 395)
+            text(".", xpassword, 400)
             textSize(20)
-            fill(255)
             xpassword += 15
 
 def keyPressed():
@@ -58,8 +64,8 @@ def keyPressed():
         pass_str = ''.join(password)
         
         userInfo = UserObject(user_str, pass_str)
-        print(userInfo)
         userInfo.convert_to_json()
+        userInfo.hash_password()
         userLoggedIn = True
         
         
--- david work/user.py
+++ david work/user.py
@@ -4,6 +4,27 @@ class UserObject(object):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        
+    def hash_password(self):
+        for i in self.password:
+            if i.isdigit():
+                hashmap = {10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F'}
+                hex_str = ''
+                    
+                if int(i) == 0:
+                    return "0"
+                
+                while int(self.password) > 0:
+                    hex_val = int(i) % 16
+                    num = int(i) // 16
+                    
+                    if hex_val >= 10:
+                        hex_str = hashmap[hex_val] + hex_str
+                    else:
+                        hex_str = str(hex_val) + hex_str    
+                print(hex_str)
+            else:
+                print(ord(i))            
     
     def convert_to_json(self):
         
@@ -15,16 +36,16 @@ class UserObject(object):
                 }
         
         jsonified_user = json.dumps(User, indent=4)
-        print(jsonified_user)
+        # print(jsonified_user)
         
         parsed_json = json.loads(jsonified_user)
                 
         file_json.write(str(jsonified_user))
         file_json.close()
-                
-        print(parsed_json["username"])        
         
-    def hash_password(self):
-        pass
+        # print(parsed_json["username"])    
+                
+    
+
 
         
